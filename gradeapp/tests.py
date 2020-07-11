b67daa0e2bec99b1adf9d7672c02e89f2f48c645
collin9726 <owino.collin@gmail.com>
2020-07-11 19:40:10 +0300

    run tests for Project model

--- gradeapp/tests.py
+++ gradeapp/tests.py
@@ -57,3 +57,39 @@ class ProfileTestClass(TestCase):
             updated_profile = prof
         self.assertEqual(updated_profile.bio, 'otherBio')
 
+
+
+class ProjectTestClass(TestCase):
+    @override_settings(MEDIA_ROOT=tempfile.gettempdir())
+    # Set up method
+    def setUp(self):
+        temp_file = tempfile.NamedTemporaryFile()
+        test_image = get_temporary_image(temp_file)            
+        self.this_user =User.objects.create_user('mimi', 'mimi@gmail.com', 'moringa')        
+        self.this_profile = Profile(bio='bio', account_holder= self.this_user, profile_photo = test_image.name)        
+        self.this_profile.save_profile()
+        self.this_project= Project(title='myproject', description='mydescription', link='https//:myproject.com', profile = self.this_profile, cover_image = test_image.name )
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Project.objects.all().delete()
+        User.objects.all().delete()
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.this_project,Project))
+
+
+    # Testing Save Method
+    def test_save_project_method(self):
+        self.this_project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+
+    # Testing Delete Method
+    def test_delete_project_method(self):
+        self.this_project.save_project()
+        project = Project.objects.get(title ='myproject')
+        project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) == 0)
